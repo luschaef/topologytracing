ebd48f9f2167c28736836753c6d83945e1409cc4
luschaef <lu.schaefer@fz-juelich.de>
2022-09-06 15:24:56 +0200

    added extract-dist-pred.pc

---
+++ extract-dist-pred.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+filename = sys.argv[1]
+basename= sys.argv[2] 
+outdir = sys.argv[3]
+n = int(sys.argv[4])
+
+data = np.load(filename, allow_pickle=True)
+lst = data.files
+
+
+dist = data['dist']
+a=np.zeros((n,n))
+
+for i in range(n):
+    for j in range(n):
+        if (i==j):
+            a[i,j]= 0
+        else:
+            a[i,j]= np.argmax(dist[i,j,1:])
+
+#plt.imshow(a)
+
+
+np.savetxt(f'{outdir}/dist-pred-{basename}.dat', a.ravel(), fmt='%d') 
+
+
