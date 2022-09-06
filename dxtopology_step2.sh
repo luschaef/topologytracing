ed39a392ad343d7fb8ec186aff7da17b2f443f17
luschaef <lu.schaefer@fz-juelich.de>
2022-09-06 15:02:27 +0200

    removed comment

--- dxtopology_step2.sh
+++ dxtopology_step2.sh
@@ -6,7 +6,7 @@ NRES="125"
 
 #calculate start weights
 ./pdb2dat.sh $OUTDIR/${BASENAME}-init-trace.pdb $OUTDIR/${BASENAME}-init-trace.dat
-#python3 extract-dist-pred.py $OUTDIR/seq.npz $BASENAME $OUTDIR $NRES
+python3 extract-dist-pred.py $OUTDIR/seq.npz $BASENAME $OUTDIR $NRES
 python3 calcmat.py $BASENAME $OUTDIR 
 python3 counts_parallel.py $BASENAME $OUTDIR
 
